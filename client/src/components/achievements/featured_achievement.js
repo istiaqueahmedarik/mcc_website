8cd8515a000894bdbcc76425c94daa997f895034
ragib100 <ragibhossain100@gmail.com>
2026-01-10 10:46:30 +0600

    first

--- client/src/components/achievements/featured_achievement.js
+++ client/src/components/achievements/featured_achievement.js
@@ -19,7 +19,6 @@ export default function FeaturedAchievement( { featuredAchievement}) {
                     />
                     <div className="absolute inset-0 bg-black/30" />
                 </div>
-
                 
                 <div className="absolute bottom-0 left-0 right-0 p-6 md:p-10 z-10">
                     <div className="max-w-4xl">
