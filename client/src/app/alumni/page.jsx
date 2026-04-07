398ce24196aca30690b9498261b2d450c5774c57
hasnat0006 <yusufrezahasnat0006@gmail.com>
2026-04-07 17:26:02 +0600

    refactor: remove fetchCache export from Achievements component and add dynamic export to AlumniPage component

--- client/src/app/alumni/page.jsx
+++ client/src/app/alumni/page.jsx
@@ -3,6 +3,8 @@ import AlumniClient from "./AlumniClient";
 
 export const metadata = { title: "Alumni | MIST Computer Club" };
 
+export const dynamic = 'force-dynamic'
+
 export default async function AlumniPage() {
   const { batches, error } = await getPublicAlumni();
   return <AlumniClient initialBatches={batches || []} loadError={error} />;
